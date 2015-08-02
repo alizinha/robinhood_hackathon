ae92e73da5491a879cd74ea086e3c71859fe8787
Jorge Reina <jorgereina1986@gmail.com>
2015-08-02 11:52:16 -0400

    practice material design

---
+++ app/src/main/java/alizinha/c4q/nyc/onemorechance/practice.java
@@ -0,0 +1,17 @@
+package alizinha.c4q.nyc.onemorechance;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class practice extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+
+    }
+
+
+}
